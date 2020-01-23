012489fd6caa102af33a0b10b83e5cdb6baada5e
Al Shariar <itmasjoy@gmail.com>
2020-01-23 07:09:16 +0600

    Added anagram folder with anagram.py file

---
+++ Anagram/anagram.py
@@ -0,0 +1,31 @@
+s1 = "fairy taless"
+s2 = "rail safety"
+
+s1 = s1.replace(" ", "").lower()
+s2 =  s2.replace(" ", "").lower()
+
+#requires n log n time (since any
+# comparision based sorting algorithm requires at least
+# n log n time to sort)
+
+print(sorted(s1) == sorted(s2))
+
+def is_anagram(s1, s2):
+    ht =  dict()
+    
+    for i in s1:
+        if i in ht:
+            ht[i] += 1
+        else:
+            ht[i] = 1
+    for i in s2:
+        if i in ht:
+            ht[i] -= 1
+        else:
+            ht[i] = 1
+    for i in ht:
+        if ht[i] != 0:
+            return False
+    return True
+
+print(is_anagram(s1, s2))
